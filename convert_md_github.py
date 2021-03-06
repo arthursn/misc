6ec3ce0a2141bd149ec48415dfbfddb65899f80b
Arthur Nishikawa <nishikawa.poli@gmail.com>
2021-03-06 01:13:37 +0100

    General improvements; eliminated dependency of temporary file

--- convert_md_github.py
+++ convert_md_github.py
@@ -11,7 +11,7 @@ import urllib.parse
 import argparse
 
 
-def translate_equations(ocl, ccl, new_ocl, new_ccl, fin, fout):
+def translate_equations(ocl, ccl, new_ocl, new_ccl, lines, encode_url=True):
     """
     Translate LaTeX equations
 
@@ -28,29 +28,36 @@ def translate_equations(ocl, ccl, new_ocl, new_ccl, fin, fout):
     fout: TextIOWrapper
         Output file stream
     """
+    new_lines = []
     len_ocl, len_ccl = len(ocl),  len(ccl)
     expr_open = False
     expr = ''
-    for line in fin:
+    for line in lines:
         while line:
             if not expr_open:
+                # Search for opening clause
                 if (m := line.find(ocl)) >= 0:
-                    fout.write(line[:m])
+                    new_lines.append(line[:m])
                     line = line[m+len_ocl:]
                     expr_open = True
                 else:
-                    fout.write(line)
+                    new_lines.append(line)
                     break
             else:
+                # Search for closing clause
                 if (m := line.find(ccl)) >= 0:
                     expr += line[:m]
-                    fout.write(new_ocl + urllib.parse.quote(expr.strip('\n')) + new_ccl)
+                    expr = expr.strip(' \n')
+                    if encode_url:
+                        expr = urllib.parse.quote(expr)
+                    new_lines.append(new_ocl + expr + new_ccl)
                     line = line[m+len_ccl:]
                     expr_open = False
                     expr = ''
                 else:
                     expr += line
                     break
+    return new_lines
 
 
 if __name__ == '__main__':
@@ -58,31 +65,50 @@ if __name__ == '__main__':
     parser.add_argument('filenames', nargs='+', help='input markdown files')
     parser.add_argument('-s', '--suffix', default='_github_compatible',
                         help='suffix of output file')
-    parser.add_argument('-e', '--extension', default='md',
-                        help='extension of output file')
+    parser.add_argument('-o', '--overwrite', action='store_true',
+                        help='overwrite file')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='activate debug mode')
     args = parser.parse_args()
+
+    if args.debug:
+        import sys
 
     render_url = 'https://render.githubusercontent.com/render/math?math='
 
     for fname in args.filenames:
-        fnametmp = fname + '.tmp'
-        fnameout = os.path.splitext(fname)[0] + args.suffix + "." + args.extension
+        lines = []
+        with open(fname, 'r') as f:
+            for line in f:
+                lines.append(line)
 
-        print(f'{fname} -> {fnameout}')
+        if args.debug:
+            # Convert inline equations. Result goes in temp file
+            lines = translate_equations('$`', '`$', '\033[1;31m', '\033[0m', lines, False)
+            # Convert block equations
+            lines = translate_equations('```math', '```', '\033[1;32m', '\033[0m', lines, False)
 
-        # Convert inline equations. Result goes in temp file
-        with open(fname, 'r') as fin, open(fnametmp, 'w') as fout:
-            translate_equations('$`', '`$',
-                                '<img src="' + render_url,
-                                '" style="display: inline; margin-top: 0;"/>',
-                                fin, fout)
+            for line in lines:
+                sys.stdout.write(line)
+        else:
+            # Convert inline equations. Result goes in temp file
+            lines = translate_equations('$`', '`$',
+                                        '<img src="' + render_url,
+                                        '" style="display: inline; margin-top: 0;"/>',
+                                        lines)
+            # Convert block equations
+            lines = translate_equations('```math', '```',
+                                        '<p align="center"><img src="' + render_url, '"/></p>',
+                                        lines)
 
-        # Convert block equations
-        with open(fnametmp, 'r') as fin, open(fnameout, 'w') as fout:
-            translate_equations('```math', '```',
-                                '<p align="center"><img src="' + render_url,
-                                '"/></p>',
-                                fin, fout)
+            if args.overwrite:
+                fnameout = fname
+                print(f'Overwriting {fname}')
+            else:
+                basename, ext = os.path.splitext(fname)
+                fnameout = basename + args.suffix + ext
+                print(f'{fname} -> {fnameout}')
 
-        # Delete temp file
-        os.remove(fnametmp)
+            with open(fnameout, 'w') as f:
+                for line in lines:
+                    f.write(line)
