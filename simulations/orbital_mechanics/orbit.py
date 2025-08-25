22d5638c2b76bd7bbfa5c38f2e7e94ac24b249dc
Arthur Nishikawa <nishikawa.poli@gmail.com>
2025-08-25 11:58:50 +0200

    Fix linting and matplotlib errors. Auto-format

--- simulations/orbital_mechanics/orbit.py
+++ simulations/orbital_mechanics/orbit.py
@@ -1,63 +1,133 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from dataclasses import dataclass
+
 import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 
-class OrbitAnimation():
-    def __init__(self, x=1, y=0, vx=0, vy=1, dt=.1):
-        self.x = x
-        self.y = y
-        self.vx = vx
-        self.vy = vy
-        self.dt = dt
+@dataclass
+class Coordinates:
+    x: float
+    y: float
 
-        self.x_orbit = [self.x]
-        self.y_orbit = [self.y]
 
-    def acceleration(self, x, y):
-        GM = 1.  # graviational constant times mass of the star
-        a = -GM/(x**2. + y**2.)**1.5
-        return a*x, a*y
+@dataclass
+class CoordinatesHistory:
+    x: list[float]
+    y: list[float]
+
+
+class Orbit:
+    GM = 1.0  # graviational constant times mass of the star
+
+    def __init__(
+        self,
+        initial_position: Coordinates = Coordinates(1, 0),
+        initial_velocity: Coordinates = Coordinates(0, 1),
+        time_step=0.1,
+    ):
+        self.position: Coordinates = initial_position
+        self.velocity: Coordinates = initial_velocity
+        self.time_step = time_step
+
+        self.position_history: CoordinatesHistory = CoordinatesHistory(
+            x=[initial_position.x],
+            y=[initial_position.y],
+        )
+
+    @staticmethod
+    def get_acceleration_gravity(position: Coordinates) -> Coordinates:
+        acceleration = -Orbit.GM / (position.x**2.0 + position.y**2.0) ** 1.5
+        return Coordinates(
+            acceleration * position.x,
+            acceleration * position.y,
+        )
+
+    def update_orbit(self):
+        acceleration = self.get_acceleration_gravity(self.position)
+
+        self.velocity.x += acceleration.x * self.time_step
+        self.velocity.y += acceleration.y * self.time_step
+
+        self.position.x += self.velocity.x * self.time_step
+        self.position.y += self.velocity.y * self.time_step
+
+        self.position_history.x.append(self.position.x)
+        self.position_history.y.append(self.position.y)
+
+
+class OrbitAnimation(Orbit):
+    def __init__(
+        self,
+        initial_position: Coordinates = Coordinates(1, 0),
+        initial_velocity: Coordinates = Coordinates(0, 1),
+        time_step=0.1,
+    ):
+        super().__init__(
+            initial_position,
+            initial_velocity,
+            time_step,
+        )
+
+        self.star: Line2D
+        self.planet: Line2D
+        self.orbit: Line2D
 
     def animate(self, *args):
-        ax, ay = self.acceleration(self.x, self.y)
-
-        self.vx += ax*self.dt
-        self.vy += ay*self.dt
-
-        self.x += self.vx*self.dt
-        self.y += self.vy*self.dt
-
-        self.x_orbit.append(self.x)
-        self.y_orbit.append(self.y)
+        self.update_orbit()
 
         # Update orbit. Plot only last 1000 points
-        self.orbit.set_data(self.x_orbit[-1000:], self.y_orbit[-1000:])
+        self.orbit.set_data(
+            self.position_history.x[-1000:],
+            self.position_history.y[-1000:],
+        )
         # Update planet position
-        self.planet.set_data(self.x_orbit[-1], self.y_orbit[-1])
+        self.planet.set_data(
+            self.position_history.x[-1:],
+            self.position_history.y[-1:],
+        )
 
         self.axis.relim()
         self.axis.autoscale_view()
 
     def run_animation(self):
         self.fig, self.axis = plt.subplots()
-        self.axis.set_aspect('equal')
+        self.axis.set_aspect("equal")
 
         # Star is at coordinates (0, 0)
-        self.star, = self.axis.plot([0], [0], 'yo', ms=10)
+        (self.star,) = self.axis.plot(
+            [0],
+            [0],
+            "yo",
+            ms=10,
+        )
 
         # Orbit
-        self.orbit, = self.axis.plot(self.x_orbit, self.y_orbit)
+        (self.orbit,) = self.axis.plot(
+            self.position_history.x,
+            self.position_history.y,
+        )
 
         # Planet
-        self.planet, = self.axis.plot(self.x_orbit[-1], self.y_orbit[-1], 'ro')
+        (self.planet,) = self.axis.plot(
+            self.position.x,
+            self.position.y,
+            "ro",
+        )
 
-        return animation.FuncAnimation(self.fig, self.animate,
-                                       interval=25, blit=False)
+        return animation.FuncAnimation(
+            self.fig,
+            self.animate,  # type: ignore
+            interval=25,
+            blit=False,
+        )
 
 
-if __name__ == '__main__':
-    orbit = OrbitAnimation(vy=.5, dt=.005)
+if __name__ == "__main__":
+    orbit_anim = OrbitAnimation(
+        initial_velocity=Coordinates(x=0, y=0.5),
+        time_step=0.005,
+    )
 
-    ani = orbit.run_animation()
+    ani = orbit_anim.run_animation()
     plt.show()
--- simulations/orbital_mechanics/orbit_multiple_bodies.py
+++ simulations/orbital_mechanics/orbit_multiple_bodies.py
@@ -1,10 +1,11 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from itertools import cycle
 
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
 
-class CelestialBody():
+
+class CelestialBody:
     def __init__(self, mass, position, velocity, history=False):
         self.mass = mass
         self.position = np.array(position)
@@ -29,7 +30,7 @@ class CelestialBody():
         self.velocity = velocity
 
 
-class PlanetarySystem():
+class PlanetarySystem:
     G = 1.0  # gravitational constant
 
     def __init__(self):
@@ -49,16 +50,18 @@ class PlanetarySystem():
             # Return index of body in self.list_bodies
             return self.nbodies - 1
         else:
-            print('{} is not a CelestialBody object'.format(body))
+            print("{} is not a CelestialBody object".format(body))
 
     def get_force_two_bodies(self, body1, body2):
         r = body1.position - body2.position
-        return self.G*body1.mass*body2.mass*r/np.linalg.norm(r)**3.
+        return self.G * body1.mass * body2.mass * r / np.linalg.norm(r) ** 3.0
 
     def get_forces_all_bodies(self):
-        for i in range(0, self.nbodies-1):
-            for j in range(i+1, self.nbodies):
-                self.matrix_forces[i, j] = self.get_force_two_bodies(self.list_bodies[i], self.list_bodies[j])
+        for i in range(0, self.nbodies - 1):
+            for j in range(i + 1, self.nbodies):
+                self.matrix_forces[i, j] = self.get_force_two_bodies(
+                    self.list_bodies[i], self.list_bodies[j]
+                )
                 self.matrix_forces[j, i] = -self.matrix_forces[i, j]
 
         self.list_forces[:, :] = self.matrix_forces.sum(axis=0)
@@ -67,29 +70,29 @@ class PlanetarySystem():
         self.get_forces_all_bodies()
 
         for i in range(self.nbodies):
-            acceleration = self.list_forces[i]/self.list_bodies[i].mass
-            velocity = self.list_bodies[i].velocity + acceleration*dt
-            position = self.list_bodies[i].position + velocity*dt
+            acceleration = self.list_forces[i] / self.list_bodies[i].mass
+            velocity = self.list_bodies[i].velocity + acceleration * dt
+            position = self.list_bodies[i].position + velocity * dt
 
             self.list_bodies[i].update_position(position)
             self.list_bodies[i].update_velocity(velocity)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     niterations = 4000
-    dt = .1
+    dt = 0.1
     plot_last = 200
 
     sun = CelestialBody(1e0, [0, 0], [0, 0], True)
-    GM_root = (PlanetarySystem.G*sun.mass)**.5
+    GM_root = (PlanetarySystem.G * sun.mass) ** 0.5
 
     # Set initial velocity that leads to an approximate circular orbit
     # mercury = CelestialBody(5e-3, [0.4, 0], [0, .9*GM_root/0.4**.5], True)
     # venus = CelestialBody(1e-4, [0.7, 0], [0, GM_root/0.7**.5], True)
-    earth = CelestialBody(1e-4, [1.0, 0], [0, GM_root/1.0**.5], True)
-    mars = CelestialBody(5e-2, [-1.5, 0], [0, -1.03*GM_root/1.5**.5], True)
-    ceres = CelestialBody(5e-2, [0, -3.0], [-0.98*GM_root/3.0**.5, 0], True)
-    jupiter = CelestialBody(1e-1, [0, 5.0], [1.05*GM_root/5.0**.5, 0], True)
+    earth = CelestialBody(1e-4, [1.0, 0], [0, GM_root / 1.0**0.5], True)
+    mars = CelestialBody(5e-2, [-1.5, 0], [0, -1.03 * GM_root / 1.5**0.5], True)
+    ceres = CelestialBody(5e-2, [0, -3.0], [-0.98 * GM_root / 3.0**0.5, 0], True)
+    jupiter = CelestialBody(1e-1, [0, 5.0], [1.05 * GM_root / 5.0**0.5, 0], True)
 
     solar_system = PlanetarySystem()
     solar_system.add_body(sun)
@@ -104,12 +107,12 @@ if __name__ == '__main__':
     for it in range(niterations):
         solar_system.calculate_dynamics(dt)
 
-    cycolors = cycle(['y', 'b', 'r', '0.5', 'm'])
+    cycolors = cycle(["y", "b", "r", "0.5", "m"])
     cysizes = cycle([4, 2, 2, 2, 2])
 
     # Setup animation
     fig, ax = plt.subplots()
-    ax.set_aspect('equal')
+    ax.set_aspect("equal")
     ax.set_xlim(-10, 10)
     ax.set_ylim(-10, 10)
 
@@ -117,9 +120,14 @@ if __name__ == '__main__':
     current_positions = []
     for body in solar_system.list_bodies:
         color = next(cycolors)
-        orbits += ax.plot(*body.position_history[:, :0], c=color, ls='-')
-        current_positions += ax.plot(*body.position_history[:, 0],
-                                     c=color, ls='none', marker='o', ms=next(cysizes))
+        orbits += ax.plot(*body.position_history[:, :0], c=color, ls="-")
+        current_positions += ax.plot(
+            *body.position_history[:, 0],
+            c=color,
+            ls="none",
+            marker="o",
+            ms=next(cysizes),
+        )
 
     def init():
         for orbit, position in zip(orbits, current_positions):
@@ -132,12 +140,18 @@ if __name__ == '__main__':
 
     def animate(it):
         for idx, body in enumerate(solar_system.list_bodies):
-            orbits[idx].set_data(*body.position_history[:, first_point[it]:it])
-            current_positions[idx].set_data(*body.position_history[:, it])
+            orbits[idx].set_data(*body.position_history[:, first_point[it] : it])
+            current_positions[idx].set_data(*body.position_history[:, it : it + 1])
         return orbits + current_positions
 
-    ani = animation.FuncAnimation(fig=fig, func=animate, frames=range(niterations),
-                                  interval=10, blit=True, init_func=init)
+    ani = animation.FuncAnimation(
+        fig=fig,
+        func=animate,
+        frames=range(niterations),
+        interval=10,
+        blit=True,
+        init_func=init,
+    )
 
     # ani.save('orbits.gif', writer='pillow')
 
