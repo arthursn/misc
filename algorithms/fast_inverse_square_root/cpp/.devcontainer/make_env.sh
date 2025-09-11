e6ed682122f4efd85cab53be70776f639b2d9e2c
Arthur Nishikawa <nishikawa.poli@gmail.com>
2025-09-12 00:30:09 +0200

    Prevent execution of make_env.sh every time

--- algorithms/fast_inverse_square_root/cpp/.devcontainer/make_env.sh
+++ algorithms/fast_inverse_square_root/cpp/.devcontainer/make_env.sh
@@ -1,5 +1,6 @@
 #/bin/sh
-echo USER=$(whoami) >.env
+USER="$(whoami)"
+echo USER=$USER >.env
 echo UID=$(id -u "$USER") >>.env
 echo GID=$(id -g "$USER") >>.env
 echo GIT_ROOT=$(git rev-parse --show-toplevel) >>.env
